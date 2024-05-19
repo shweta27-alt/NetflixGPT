a5b0c71fa8d54049cc207769867a395fa9c51b27
Shweta Rajendra Saindane <80962110+shweta27-alt@users.noreply.github.com>
2024-05-19 23:50:30 +0530

    Api fetch for different types

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,12 +4,14 @@ import MovieCard from "./MovieCard";
 const MovieList = ({ title, movies }) => {
   if (!movies) return;
   return (
-    <div>
-      <h1>{title}</h1>
-      <div className="flex">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} posterPath={movie.poster_path} />
-        ))}
+    <div className="px-6 bg-transparant">
+      <h1 className="text-3xl py-4 text-white">{title}</h1>
+      <div className="flex overflow-x-scroll">
+        <div className="flex">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} posterPath={movie.poster_path} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import MovieList from './MovieList'
-import { useSelector } from 'react-redux'
+import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
-  const movies = useSelector(store => store.movies)
+  const movies = useSelector((store) => store.movies);
   return (
-    <div>
-      <MovieList title={"Now Playing"} movies = {movies.nowPlayingMovies}/>
-    </div>
-  )
-}
+    movies && (
+      <div className="bg-black">
+        <div className="-mt-52 pl-6 relative z-20">
+        <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
+        <MovieList title={"Popular"} movies={movies.popularMovies} />
+        <MovieList title={"Upcoming"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
+        </div>
+      </div>
+    )
+  );
+};
 
-export default SecondaryContainer
+export default SecondaryContainer;
